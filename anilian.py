33a90ace072252c08c4263b01dd9bd6eeba60dd2
TIAGO250210 <85694351+TIAGO250210@users.noreply.github.com>
2022-10-03 23:09:18 -0300

    Update anilian.py

--- anilian.py
+++ anilian.py
@@ -92,17 +92,4 @@ if __name__ == '__main__':
 
     print(sol)
     print(val)
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
